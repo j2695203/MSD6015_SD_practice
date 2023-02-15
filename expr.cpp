ef8f7b000f061f9b516ea94f2755dfe170a46e28
Jinny Jeng <j2695203@gmail.com>
2023-02-14 20:13:00 -0700

    update test cases

--- expr.cpp
+++ expr.cpp
@@ -427,12 +427,15 @@ void Let::pretty_print_at(std::ostream& ost, precedence_t p, int acc, long lastL
     long newLinePos = ost.tellp();
     
     // blank
+    // long space = letPos-lastLinePos;
+    // os<<std::string(space, ' ');
     for(int i = 0; i < letPos - (lastLinePos) ; i++){
         ost<< " ";
     }
     
+    
     // _in
-    ost<< "_in ";
+    ost<< "_in  ";
     body->pretty_print_at(ost, prec_none, acc, newLinePos );
     
     
@@ -448,62 +451,244 @@ void Let::pretty_print_at(std::ostream& ost, precedence_t p, int acc, long lastL
  *** TEST using Catch 2 framework ************************************************
  */
 
+
+/*
+ Kevin & Will's test
+ */
+
+TEST_CASE("Pretty Print examples_Kevin"){           //Created from assignment examples
+    
+    CHECK((new Let("x", new Num(5), new Add(new Var("x"), new Num(1))))
+          ->pretty_print_to_string() == "_let x = 5\n"
+                                     "_in  x + 1");
+
+    CHECK((new Add(new Let("x", new Num(5), new Var("x")), new Num(1)))
+          ->pretty_print_to_string()== "(_let x = 5\n"
+                                    " _in  x) + 1");
+
+    CHECK((new Mult(new Num(5), new Let("x", new Num(5), new Add(new Var("x"), new Num(1)))))
+          ->pretty_print_to_string() == "5 * _let x = 5\n"
+                                     "    _in  x + 1");
+
+    CHECK((new Add(new Mult(new Num(5), new Let("x", new Num(5), new Var("x"))), new Num(1)))
+          ->pretty_print_to_string() == "5 * (_let x = 5\n"
+                                     "     _in  x) + 1");
+
+    CHECK((new Let("x", new Num(5), new Add(new Let("y", new Num(3), new Add(new Var("y"), new Num(2))), new Var("x"))))
+          ->pretty_print_to_string() == "_let x = 5\n"
+                                     "_in  (_let y = 3\n"
+                                     "      _in  y + 2) + x");
+
+    CHECK((new Let("x", new Let("y", new Num(6), new Mult(new Var("y"), new Num(2))), new Add(new Var("x"), new Num(1))))
+          ->pretty_print_to_string() == "_let x = _let y = 6\n"
+                                     "         _in  y * 2\n"
+                                     "_in  x + 1");
+}
+
+TEST_CASE("pretty_print_let_mine_some_reuse_of_kevin_triple_nested_let") {
+    Let* tripleNestedLet = new Let("x", new Num(1),
+                                   new Let("y", new Num(1),
+                                           new Mult(new Add(new Var("x"), new Var("y")), new Var("z"))));
+    Let* tripleNestedLet2 = new Let("x", new Num(1),
+                                    new Let("y", new Num(1),
+                                            new Let("z", new Add(new Var("x"), new Num(1)),
+                                                    new Add(new Add(new Var("x"), new Var("y")), new Var("z")))));
+    Let* tripleNestedLet3 = new Let("x", new Num(1),
+                                    new Let("y", new Num(1),
+                                            new Let("z", new Add(new Var("x"), new Num(1)),
+                                                    new Mult(new Add(new Var("x"), new Var("y")), new Var("z")))));
+
+
+    CHECK(tripleNestedLet -> pretty_print_to_string() ==
+          "_let x = 1\n"
+          "_in  _let y = 1\n"
+          "     _in  (x + y) * z"
+    );
+    CHECK(tripleNestedLet2 -> pretty_print_to_string() ==
+          "_let x = 1\n"
+          "_in  _let y = 1\n"
+          "     _in  _let z = x + 1\n"
+          "          _in  (x + y) + z"
+    );
+    CHECK(tripleNestedLet3 -> pretty_print_to_string() ==
+                            "_let x = 1\n"
+                            "_in  _let y = 1\n"
+                            "     _in  _let z = x + 1\n"
+                            "          _in  (x + y) * z"
+    );
+    Let* tripleNestedLet4 =new Let("x", new Num(5),
+                                   new Let("y", new Num(3),
+                                           new Add(new Var("y"), new Num(2))));
+    Let* tripleNestedLet5 =new Let("x", new Num(5),
+                                   new Add(new Let("y", new Num(3),
+                                                   new Add(new Var("y"), new Num(2))), new Var("x")));
+
+    CHECK(tripleNestedLet4->pretty_print_to_string() ==
+                  "_let x = 5\n"
+                  "_in  _let y = 3\n"
+                  "     _in  y + 2"
+    );
+
+    CHECK(tripleNestedLet5 -> pretty_print_to_string() == "_let x = 5\n"
+                                                      "_in  (_let y = 3\n"
+                                                      "      _in  y + 2) + x");
+    SECTION("assignment_examples") {
+        CHECK( (new Add(new Mult(new Num(5), new Let("x", new Num(5), new Var("x"))), new Num(1)))-> pretty_print_to_string()
+               == "5 * (_let x = 5\n"
+                  "     _in  x) + 1");
+        CHECK( (new Mult(new Mult(new Num (2), new Let("x", new Num(5), new Add(new Var("x") ,new  Num(1)) )), new Num(3) )) -> pretty_print_to_string()
+               == "(2 * _let x = 5\n"
+                  "     _in  x + 1) * 3");
+    }
+    // A _let needs parentheses when it is nested immediately as the right argument of an unparenthesized *
+    // where _let would have needed parentheses in the surrounding context
+    // (that is, if the _let used in place of the whole * would need parentheses,
+    // then it still needs parentheses within the right-hand size of *).
+    SECTION("new_edge") {
+        CHECK( (new Mult(new Num (2), new Let("x", new Num(5), new Add(new Var("x") ,new  Num(1)) ) )) -> pretty_print_to_string()
+               == "2 * _let x = 5\n"
+                  "    _in  x + 1");
+        CHECK( (new Add(new Mult(new Num(5), new Let("x", new Num(5), new Mult(new Var("x"), new Num(2)))), new Num(1)))-> pretty_print_to_string()
+               == "5 * (_let x = 5\n"
+                  "     _in  x * 2) + 1");
+        CHECK( (new Mult((new Add(new Mult(new Num(5), new Let("x", new Num(5), new Mult(new Var("x"), new Num(2)))), new Num(1))), new Num(7)))-> pretty_print_to_string()
+               == "(5 * (_let x = 5\n"
+                  "      _in  x * 2) + 1) * 7");
+        CHECK( (new Let("x", new Num(10), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Num(10))), new Mult(new Num(10), new Num(10)))))
+                       ->pretty_print_to_string()  == "_let x = 10\n"
+                                                      "_in  (x * (10 * 10) * 10) * 10 * 10");
+        CHECK( (new Let("x", new Num(1), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Var("x"))), new Mult(new Num(10), new Num(10)))))
+                        -> pretty_print_to_string() == "_let x = 1\n"
+                                                       "_in  (x * (10 * 10) * x) * 10 * 10" );
+        CHECK( (new Let("x", new Num(1), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Var("x"))), new Mult(new Var("y"), new Num(10)))))
+                       -> pretty_print_to_string() == "_let x = 1\n"
+                                                      "_in  (x * (10 * 10) * x) * y * 10" );
+    }
+}
+
+TEST_CASE("Let_equals_mine") {
+        SECTION("Values_same") {
+            REQUIRE( (new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->equals(new Let("x", new Num(4), new Add(new Num(2), new Var("x")) )));
+        }
+        SECTION("Values_same_different_rhs") {
+            REQUIRE( !(new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->equals(new Let("x", new Num(5), new Add(new Num(2), new Var("x")) )));
+        }
+        SECTION("Values_same_different_lhs") {
+            REQUIRE( !(new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->equals(new Let("y", new Num(4), new Add(new Num(2), new Var("x")) )));
+        }
+        SECTION("Values_same_different_body") {
+            REQUIRE( !(new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->equals(new Let("x", new Num(4), new Mult(new Num(3), new Var("y")) )));
+        }
+        SECTION("different_types") {
+            REQUIRE( !(new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->equals( new Mult(new Num(3), new Var("y")) ));
+        }
+}
+
+TEST_CASE("Let_has_variable_mine") {
+    SECTION("has") {
+        REQUIRE( (new Let("x", new Num(4), new Add(new Num(2), new Var("x")) ))->has_variable());
+    }
+    SECTION("does_not_has") {
+        REQUIRE( !(new Let("x", new Num(4), new Add(new Num(2), new Num(4)) ))->has_variable());
+    }
+}
+TEST_CASE("Let_print_mine") {
+    CHECK( (new Let("x", new Num(5), new Add(new Let("y", new Num(3), new Add(new Var("y"), new Num(2))), new Var("x")))) -> to_string()
+                                                                                                        == "(_let x=5 _in ((_let y=3 _in (y+2))+x))");
+    CHECK( (new Let("x", new Num(1),
+                   new Let("y", new Num(1),
+                           new Let("z", new Add(new Var("x"), new Num(1)),
+                                   new Mult(new Add(new Var("x"), new Var("y")), new Var("z")))))) -> to_string()
+                                                                                                        == "(_let x=1 _in (_let y=1 _in (_let z=(x+1) _in ((x+y)*z))))");
+}
+TEST_CASE ("Let_interp_mine") {
+    SECTION("hw_examples") {
+        CHECK((new Add(new Mult(new Num(5), new Let("x", new Num(5), new Var("x"))), new Num(1))) -> interp() == 26);
+        CHECK((new Mult(new Num(5), new Let("x", new Num(5), new Add(new Var("x"), new Num(1))))) -> interp() == 30);
+    }
+    SECTION("from_pretty_print_edge") {
+        CHECK( (new Let("x", new Num(1),
+                       new Let("y", new Num(1),
+                               new Let("z", new Add(new Var("x"), new Num(1)),
+                                       new Mult(new Add(new Var("x"), new Var("y")), new Var("z")))))) -> interp() == 4);
+        CHECK( (new Mult((new Add(new Mult(new Num(5), new Let("x", new Num(5), new Mult(new Var("x"), new Num(2)))), new Num(1))), new Num(7))) -> interp() == 357); // 51 * 7
+        CHECK( (new Let("x", new Num(10), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Num(10))), new Mult(new Num(10), new Num(10)))))
+                      ->interp()  == 1000000);
+        CHECK( (new Let("x", new Num(1), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Var("x"))), new Mult(new Num(10), new Num(10)))))
+                       ->interp()  == 10000);
+        CHECK_THROWS_WITH( ((new Let("x", new Num(1), new Mult( new Mult(new Var("x"), new Mult(new Mult(new Num(10), new Num(10)), new Var("x"))), new Mult(new Var("y"), new Num(10)))))
+                       -> interp() == 10000), "no value for variable");
+    }
+    SECTION("bypass_middle_let") {
+        CHECK ((new Let("x", new Num(2), new Let("z", new Num(4), new Add(new Var("x"), new Num(10)))))
+                -> interp() == 12);
+    }
+}
+
+
+
+/*
+ Jinny's test
+ */
+
 TEST_CASE("Let_pretty_print_to_string (includes pretty_print)"){
     
     SECTION("let in left"){
         CHECK((new Add (new Let("x",new Num(5), new Var("x")) , new Num(1)))
               ->pretty_print_to_string() == "(_let x = 5"    "\n"
-                                         " _in x) + 1");
+                                         " _in  x) + 1");
         CHECK((new Mult( new Num(5), new Let("x",new Num(5), new Add(new Var("x"),new Num(1)))))
               ->pretty_print_to_string() == "5 * _let x = 5"   "\n"
-                                         "    _in x + 1");
+                                         "    _in  x + 1");
     }
     
     SECTION("let in right"){
         CHECK((new Add (new Num(1), new Let("x",new Num(5), new Var("x"))))
               ->pretty_print_to_string() == "1 + _let x = 5"    "\n"
-                                         "    _in x");
+                                         "    _in  x");
     }
     
     SECTION("let in middle"){
         CHECK((new Add (new Mult( new Num(5), new Let("x",new Num(5), new Var("x"))) , new Num(2)))
               ->pretty_print_to_string() == "5 * (_let x = 5"   "\n"
-                                         "     _in x) + 2");
+                                         "     _in  x) + 2");
         CHECK((new Mult (new Mult( new Num(5), new Let("x",new Num(5), new Var("x"))) , new Num(2)))
               ->pretty_print_to_string() == "(5 * _let x = 5"   "\n"
-                                         "     _in x) * 2");
+                                         "     _in  x) * 2");
         CHECK((new Mult (new Add( new Num(5), new Let("x",new Num(5), new Var("x"))) , new Num(2)))
               ->pretty_print_to_string() == "(5 + _let x = 5"   "\n"
-                                         "     _in x) * 2");
+                                         "     _in  x) * 2");
         CHECK((new Mult (new Add(new Let("x",new Num(5), new Var("x")), new Num(5)) , new Num(2)))
               ->pretty_print_to_string() == "((_let x = 5"   "\n"
-                                         "  _in x) + 5) * 2");
+                                         "  _in  x) + 5) * 2");
     }
     
     SECTION("nested"){
+        
         CHECK(( new Let("x", new Num(5), new Let("x", new Num(3), new Add(new Var("x"), new Num(1)))) )
               ->pretty_print_to_string() == "_let x = 5"      "\n"
-                                         "_in _let x = 3"   "\n"
-                                         "    _in x + 1");
+                                         "_in  _let x = 3"   "\n"
+                                         "     _in  x + 1");
         CHECK(( new Let("x", new Num(5) , new Let("x", new Add( new Var("x"), new Num(5)), new Add(new Var("x"), new Num(1)))) )
               ->pretty_print_to_string() == "_let x = 5"      "\n"
-                                         "_in _let x = x + 5"   "\n"
-                                         "    _in x + 1");
+                                         "_in  _let x = x + 5"   "\n"
+                                         "     _in  x + 1");
         
         CHECK(( new Let("x", new Num(5), new Add(new Let("x", new Num(3), new Add(new Var("x"), new Num(1))), new Num(2)) ) )
               ->pretty_print_to_string() == "_let x = 5"      "\n"
-                                         "_in (_let x = 3"   "\n"
-                                         "     _in x + 1) + 2");
+                                         "_in  (_let x = 3"   "\n"
+                                         "      _in  x + 1) + 2");
         
         CHECK(( new Let("x", new Num(5), new Add(new Num(2), new Let("x", new Num(3), new Add(new Var("x"), new Num(1))) ) ) )
               ->pretty_print_to_string() == "_let x = 5"      "\n"
-                                         "_in 2 + _let x = 3"   "\n"
-                                         "        _in x + 1");
+                                         "_in  2 + _let x = 3"   "\n"
+                                         "         _in  x + 1");
     }
 }
 
 
 TEST_CASE("Let_all methods"){
+    
     SECTION("toString (includes print)"){
         CHECK((new Let("x", new Num(5), new Add(new Var("x"), new Num(1))) )
               ->to_string() == "(_let x=5 _in (x+1))");
